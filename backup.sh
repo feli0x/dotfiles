fc2646b1f79cb213b11a56240c2e1b29d7d2d05a
Felix Swinkels <tgifelix@icloud.com>
2020-11-10 11:15:45 +0100

    automatic update:

--- backup.sh
+++ backup.sh
@@ -1,9 +1,13 @@
-!/usr/bin/env bash
+#!/bin/sh
 
 # copy dotfiles
 cp ~/.zshrc ~/dotfiles/macos/.zshrc
 cp ~/.vimrc ~/dotfiles/macos/.vimrc
 
 # push
-
-
+if [[ `git status --porcelain` ]]; then
+	git pull origin master
+	git add .
+	git commit -m "automatic update: $(timestamp)"
+	git push origin master
+fi
