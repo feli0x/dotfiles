dc205faccf3a460a4394d0dea440a80a2750f686
Felix Swinkes <tgifelix@icloud.com>
2021-02-06 01:26:12 +0100

    Update: 06-02-2021 @ 01:26:12

--- old/setup/brew.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew tap homebrew/cask-versions
-brew install node npm neofetch onefetch gatsby-cli sass yarn tree nmap rtv serve htop
-brew cask install iterm2 visual-studio-code macvim spotify slack google-chrome brave-browser firefox-developer-edition browserosaurus vlc github raspberry-pi-imager kap 
-
