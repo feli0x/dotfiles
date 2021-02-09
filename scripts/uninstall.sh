e8d36687469ae6b6790dd1491dd7a12f62c35756
Felix Swinkes <tgifelix@icloud.com>
2021-02-09 13:57:16 +0100

    uninstall script

---
+++ scripts/uninstall.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+color_off='\033[0m'
+color1='\033[0;35m'
+color2='\033[1;36m'
+dir=$PWD
+nuke='$HOME/dotfiles $HOME/.zsh $HOME/.aliases $HOME/.debian $HOME/.macos $HOME/.zshrc'
+# }}}
+fancyprint () {
+    printf '%b\n' "$1$2$color_off" >&2
+}
+# }}}
+clear
+echo ''
+fancyprint ${color1} ' ██████╗  ██████╗ ████████╗███████╗██╗██╗     ███████╗███████╗'
+fancyprint ${color1} ' ██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝██║██║     ██╔════╝██╔════╝'
+fancyprint ${color1} ' ██║  ██║██║   ██║   ██║   █████╗  ██║██║     █████╗  ███████╗'
+fancyprint ${color1} ' ██║  ██║██║   ██║   ██║   ██╔══╝  ██║██║     ██╔══╝  ╚════██║'
+fancyprint ${color1} ' ██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████║'
+fancyprint ${color1} ' ╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝'
+fancyprint ${color2} ' https://github.com/tgifelix/dotfiles'
+echo ''
+# }}}
+fancyprint ${color2} ' >> Nuking: $nuke <<'
+rm -rf $nuke
+fancyprint ${color2} 'Reinstall with: git clone https://github.com/tgifelix/dotfiles "$HOME/dotfiles" && bash $HOME/dotfiles/scripts/bootstrap.sh'
