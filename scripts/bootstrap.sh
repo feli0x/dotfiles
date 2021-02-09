40c76eaec7b010edfd62a34a9078ad647757c60d
Felix Swinkes <tgifelix@icloud.com>
2021-02-09 13:57:28 +0100

    dramatics

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,22 +1,41 @@
 #!/bin/sh
+color_off='\033[0m'
+color1='\033[0;35m'
+color2='\033[1;36m'
+dir=$PWD
+# }}}
+fancyprint () {
+    printf '%b\n' "$1$2$color_off" >&2
+}
+# }}}
 clear
-
-echo ' >> Cloning Plugins <<'
+echo ''
+fancyprint ${color1} ' ██████╗  ██████╗ ████████╗███████╗██╗██╗     ███████╗███████╗'
+fancyprint ${color1} ' ██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝██║██║     ██╔════╝██╔════╝'
+fancyprint ${color1} ' ██║  ██║██║   ██║   ██║   █████╗  ██║██║     █████╗  ███████╗'
+fancyprint ${color1} ' ██║  ██║██║   ██║   ██║   ██╔══╝  ██║██║     ██╔══╝  ╚════██║'
+fancyprint ${color1} ' ██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████║'
+fancyprint ${color1} ' ╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝'
+fancyprint ${color2} ' https://github.com/tgifelix/dotfiles'
+echo ''
+# }}}
+fancyprint ${color2} ' >> Cloning Plugins <<'
 git clone https://github.com/geometry-zsh/geometry $HOME/.zsh/geometry
 git clone https://github.com/marlonrichert/zsh-autocomplete.git $HOME/.zsh/zsh-autocomplete
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
 git clone https://github.com/sindresorhus/pure.git $HOME/.zsh/pure
-
+# }}}
 clear
-
-echo ' >> Copying DotFiles <<'
+# }}}
+echo ''
+fancyprint ${color2} ' >> Copying DotFiles <<'
 cp $HOME/dotfiles/.aliases $HOME/.aliases
 cp $HOME/dotfiles/.debian $HOME/.debian
 cp $HOME/dotfiles/.macos $HOME/.macos
 cp $HOME/dotfiles/.zshrc $HOME/.zshrc
-
+# }}}
 clear
-
-echo 'Done! Reloading ZShell...'
+# }}}
+fancyprint ${color2} ' >> Done! Reloading ZShell... <<'
 sleep 3
 zsh
