dc67109adb5f99451172372727704860e8cee7e9
Felix Swinkes <tgifelix@icloud.com>
2021-02-09 13:08:13 +0100

    bootstrap

--- scripts/bootstrapz.sh
+++ scripts/bootstrapz.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+clear
+
 echo ' >> Cloning Plugins <<'
 git clone https://github.com/geometry-zsh/geometry $HOME/.zsh/geometry
 git clone https://github.com/marlonrichert/zsh-autocomplete.git $HOME/.zsh/zsh-autocomplete
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
+git clone https://github.com/sindresorhus/pure.git $HOME/.zsh/pure
+
+clear
 
 echo ' >> Copying DotFiles <<'
 cp $HOME/dotfiles/.aliases $HOME/.aliases
@@ -10,5 +15,8 @@ cp $HOME/dotfiles/.debian $HOME/.debian
 cp $HOME/dotfiles/.macos $HOME/.macos
 cp $HOME/dotfiles/.zshrc $HOME/.zshrc
 
+clear
+
 echo 'Done!'
+
 exit 0
